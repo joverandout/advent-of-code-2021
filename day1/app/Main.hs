d17c6e191c5e9022621a9c9c1c3c4b0341947201
joverandout <your_github_email>
2021-12-01 22:39:24 +0000

    added input and tidied up the haskell code

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -12,8 +12,8 @@ prepare :: String -> Input
 prepare = map read . lines
 
 part1 :: Input -> Int
-part1 [x] = 0
 part1 (x:y:xs) = sub x y + part1 (y:xs)
+part1 xs = 0
 
 part2 :: Input -> Int
 part2 (x:y:z:w:xs) = sub (x + y + z) (y + z + w) + part2 (y:z:w:xs)
