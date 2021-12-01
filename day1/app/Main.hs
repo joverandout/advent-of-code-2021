fb5cc7b7e4b3a4e85213754eb33826c0410bdc0a
joverandout <joverandout@gmail.com>
2021-12-01 19:28:20 +0000

    day 1 both parts done

---
+++ day1/app/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Lib
+import Control.Arrow
+
+type Input = [Int]
+
+main :: IO ()
+main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
+
+prepare :: String -> Input
+prepare = map read . lines
+
+part1 :: Input -> Int
+part1 [x] = 0
+part1 (x:y:xs) = sub x y + part1 (y:xs)
+
+part2 :: Input -> Int
+part2 (x:y:z:w:xs) = sub (x + y + z) (y + z + w) + part2 (y:z:w:xs)
+part2 xs = 0
+
+sub :: Int -> Int -> Int
+sub x y = fromEnum (y > x)
