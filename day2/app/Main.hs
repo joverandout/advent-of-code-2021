b2c0a53eeb70e3e1fc52bc8e5cf68273600cb409
joverandout <your_github_email>
2021-12-02 13:14:16 +0000

    both parts working

--- day2/app/Main.hs
+++ day2/app/Main.hs
@@ -4,25 +4,34 @@ import Lib
 import Control.Arrow         
 
 data Direction = Up | Down | Forward deriving Show
-data Motion = Motion Direction Int deriving Show
-type Coordinates = (Int, Int)
-
+data Instruction = Instruction Direction Int deriving Show
+type Coordinates1 = (Int, Int)
+type Coordinates2 = (Int, Int ,Int)
 
 main :: IO ()
-main = do readFile "input.txt" >>= print . part1 . prepare
+main = do readFile "input.txt" >>= print . (part1 &&& part2) . prepare
 
-part1 :: [Motion] -> Int
+part1 :: [Instruction] -> Int
 part1 instructions = x * y where (x, y) = foldl updateCoords1 (0,0) instructions
 
-updateCoords1 :: Coordinates -> Motion -> Coordinates
-updateCoords1 (x,y) (Motion m n) = case m of
+part2 :: [Instruction] -> Int
+part2 instructions = x * y where (x, y, z) = foldl updateCoords2 (0,0,0) instructions
+
+updateCoords1 :: Coordinates1 -> Instruction -> Coordinates1
+updateCoords1 (x,y) (Instruction m n) = case m of
     Forward -> (x+n, y)
     Up -> (x, y-n)
     Down -> (x, y+n)
 
-prepare :: String -> [Motion]
+updateCoords2 :: Coordinates2 -> Instruction -> Coordinates2
+updateCoords2 (x, y, z) (Instruction m n) = case m of
+    Forward -> (x+n, y+(z * n), z)
+    Up -> (x, y, z-n)
+    Down -> (x, y, z+n)
+
+prepare :: String -> [Instruction]
 prepare = map (parse . words) . lines
-    where parse [direction, val] = Motion d (read val)
+    where parse [direction, val] = Instruction d (read val)
             where d = case direction of
                     "down" -> Down
                     "up" -> Up
