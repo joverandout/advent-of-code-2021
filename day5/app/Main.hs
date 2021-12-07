50af893ba151ef1c2fe66f1a510eec4806bb5e70
joverandout <joverandout@gmail.com>
2021-12-07 18:40:56 +0000

    curry eaten and part 2 done easy

--- day5/app/Main.hs
+++ day5/app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Data.List.Split (splitOn)
 import qualified Data.Map.Strict as Map
 import Lib
+import Control.Arrow
+
 
 type Point = (Int, Int)
 type Line = (Point, Point)
@@ -16,21 +18,31 @@ vertical ((x1, y1), (x2, y2)) = x1 == x2
 horizontal :: Line -> Bool
 horizontal ((x1, y1), (x2, y2)) =  y1 == y2
 
+diagonal :: Line -> Bool
+diagonal ((x1, y1), (x2, y2)) = abs (x1-x2) == abs (y1-y2)
+
 prepare :: String -> Input
 prepare = map parseLine . lines
 
 parseLine :: String -> Line
-parseLine = first . map parsePoint . splitOn " -> "
+parseLine = firstVal . map parsePoint . splitOn " -> "
 
 parsePoint :: String -> Point
-parsePoint = first . map read . splitOn ","
+parsePoint = firstVal . map read . splitOn ","
 
-first :: [b] -> (b,b)
-first (a:b:xs) = (a,b)
-first [a] = error "First can only work on a list of size 2 or more"
+firstVal :: [b] -> (b,b)
+firstVal (a:b:xs) = (a,b)
+firstVal [a] = error "firstVal can only work on a list of size 2 or more"
 
-getLineCoverage :: Line -> [Point]
-getLineCoverage line@((x1, y1), (x2, y2))
+getLineCoverage2 :: Line -> [Point]
+getLineCoverage2 line@((x1, y1), (x2, y2))
+                | vertical line = getCoverageOfVerticalLine line (y1 > y2) (y1 == y2)
+                | horizontal line = getCoverageOfHorizontalLine line (x1 > x2) (x1 == x2)
+                | diagonal line = getCoverageOfDiagonalLine line
+                | otherwise = []
+
+getLineCoverage1 :: Line -> [Point]
+getLineCoverage1 line@((x1, y1), (x2, y2))
                 | vertical line = getCoverageOfVerticalLine line (y1 > y2) (y1 == y2)
                 | horizontal line = getCoverageOfHorizontalLine line (x1 > x2) (x1 == x2)
                 | otherwise = []
@@ -45,16 +57,34 @@ getCoverageOfHorizontalLine ((x1, y1), (x2, y2)) _ True = [(x1, y2)]
 getCoverageOfHorizontalLine ((x1, y1), (x2, y2)) True False  = (x1, y1) : getCoverageOfHorizontalLine ((x1 - 1, y1), (x2, y2)) True  ((x1 - 1) == x2)
 getCoverageOfHorizontalLine ((x1, y1), (x2, y2)) False False = (x1, y1) : getCoverageOfHorizontalLine ((x1 + 1, y1), (x2, y2)) False ((x1 + 1) == x2)
 
+getCoverageOfDiagonalLine :: Line -> [Point]
+getCoverageOfDiagonalLine ((x1, y1), (x2, y2)) =
+    let xDirection = if x1 < x2 then 1 else -1
+        yDirection = if y1 < y2 then 1 else -1
+        difference = abs (x2-x1)
+        x = x1
+        y = y1
+    in map (\d -> (x1 + (d * xDirection), y1 + (d * yDirection))) [0 .. difference]
+
 part1 :: Input -> Int
 part1 input = 
-    let coverage = foldl addLineToMap Map.empty input
+    let coverage = foldl addLineToMap1 Map.empty input
+    in length $ filter (\(_, v) -> v > 1) $ Map.toList coverage
+
+part2 :: Input -> Int
+part2 input = 
+    let coverage = foldl addLineToMap2 Map.empty input
     in length $ filter (\(_, v) -> v > 1) $ Map.toList coverage
 
 addPointToMap :: LineCoverage -> Point -> LineCoverage
 addPointToMap coverage point = Map.insertWith (+) point 1 coverage
 
-addLineToMap :: LineCoverage -> Line -> LineCoverage
-addLineToMap coverage = foldl addPointToMap coverage . getLineCoverage
+addLineToMap1 :: LineCoverage -> Line -> LineCoverage
+addLineToMap1 coverage = foldl addPointToMap coverage . getLineCoverage1
+
+
+addLineToMap2 :: LineCoverage -> Line -> LineCoverage
+addLineToMap2 coverage = foldl addPointToMap coverage . getLineCoverage2
 
 main :: IO ()
-main = readFile "input.txt" >>= print . part1 . prepare
+main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
