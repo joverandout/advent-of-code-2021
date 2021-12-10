98083faf75215ad6dea2d7a0c15405761e099328
joverandout <joverandout@gmail.com>
2021-12-10 13:07:36 +0000

    this one is much harder than we first thought

--- day9/app/Main.hs
+++ day9/app/Main.hs
@@ -3,28 +3,48 @@ module Main where
 import Data.List.Split (splitOn)
 import Control.Arrow
 
-type Input = [Integer]
+zeros =  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+arrayTest = [[2,1,9,9,9,4,3,2,1,0], [3,9,8,7,8,9,4,9,2,1], [9,8,5,6,7,8,9,8,9,2], [8,7,6,7,8,9,6,7,8,9], [9,8,9,9,9,6,5,6,7,8]]
+zerosTest = [0,0,0,0,0,0,0,0,0,0]
 
 main :: IO ()
-main = readFile "input.txt" >>= print . {-(part1 &&& part2) .-} prepare
+main = readFile "input.txt" >>= print . part1 . prepare3 . prepare
 
-prepare :: String -> Input
+sumVals :: [Integer] -> Integer
+sumVals xs = sum (map (+1) xs)
+
+prepare :: String -> [Integer]
 prepare = map read . lines
+
+prepare3 :: [Integer] -> [[Integer]]
+prepare3 [x] = [prepare2 x]
+prepare3 (x:xs) = prepare2 x : prepare3 xs
+
+prepare2 :: Integer -> [Integer]
+prepare2 0 = []
+prepare2 x = prepare2 (x `div` 10) ++ [x `mod` 10]
 
 longIntToArray :: Integral x => x -> [x]
 longIntToArray 0 = []
 longIntToArray x = longIntToArray (x `div` 10) ++ [x `mod` 10]
 
-arrayToLows :: [Int] -> Int -> [Int]
-arrayToLows [x] y
-            | x < y = [x]
-            | otherwise = []
-arrayToLows (x:xs) y
-            | x < y && x < head xs =  x : arrayToLows xs x
-            | otherwise = arrayToLows xs x
+runArraysToLows :: [[Integer]] -> [Integer] -> [Integer]
+runArraysToLows [] lastRun = []
+runArraysToLows [x] lastRun = arrayToLows lastRun x zerosTest 9999999
+runArraysToLows (x:xs) lastRun = arrayToLows lastRun x (head xs) 9999999 ++ runArraysToLows xs x
 
--- part1 :: Input -> Int
--- part1 input = const()
+arrayToLows :: [Integer] -> [Integer] -> [Integer] -> Integer -> [Integer]
+arrayToLows [z] [x] [w] y
+            | x < y && x < z && x < w = [x]
+            | otherwise = []
+arrayToLows (z:zs) (x:xs) (w:ws) y
+            | x < y && x < head xs && x < z && x < w =  x : arrayToLows zs xs ws x
+            | otherwise = arrayToLows zs xs ws x
+arrayToLows _ [] _ _ = []
+
+
+part1 :: [[Integer]] -> Integer
+part1 input = sumVals (runArraysToLows input zeros)
 
 -- part2 :: Input -> Int
 -- part2 input = const()
