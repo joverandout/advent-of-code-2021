11769136883d1e88ec6d4fe64783273156e6fec6
joverandout <joverandout@gmail.com>
2021-12-19 23:57:00 +0000

    not optimised because I'm lazy and hungryyyy

--- day06/app/Main.hs
+++ day06/app/Main.hs
@@ -12,6 +12,12 @@ main = readFile "input.txt" >>= print . part1 . prepare
 prepare :: String -> Input
 prepare = map read . splitOn ","
 
+part1 :: Input -> Int
+part1 fishes = length (ageSchoolNTimes 80 fishes)
+
+part2 :: Input -> Int
+part2 fishes = length (ageSchoolNTimes 256 fishes)
+
 updateFish :: Fish -> [Int]
 updateFish 0 = [6,8]
 updateFish x = [x-1]
@@ -22,9 +28,3 @@ updateFishes (x:xs) = updateFish x ++ updateFishes xs
 
 ageSchoolNTimes :: Int -> [Fish] -> [Fish]
 ageSchoolNTimes n school = iterate updateFishes school !! n
-
-part1 :: Input -> Int
-part1 fishes = length (ageSchoolNTimes 80 fishes)
-
-part2 :: Input -> Int
-part2 fishes = length (ageSchoolNTimes 256 fishes)
