588cc4e2b1bca47330c4bcc422e4693392a4ba1a
joverandout <joverandout@gmail.com>
2021-12-20 00:04:33 +0000

    even after a few baileys wasn't piece of piddle

--- day12/app/Main.hs
+++ day12/app/Main.hs
@@ -28,6 +28,23 @@ buildGraph (nodes, edges) (from, to) =
 emptyGraph :: Graph
 emptyGraph = (Set.empty, Map.empty)
 
+visitable :: [Node] -> Node -> Bool
+visitable path node
+  | nodeLarge node = True
+  | node == "start" = False
+  | otherwise =
+    if hasRepeatingElement (filter (not . nodeLarge) path)
+      then node `notElem` path
+      else isNotInOrOnce path node
+
+hasRepeatingElement :: (Ord a) => [a] -> Bool
+hasRepeatingElement xs = (xs & Set.fromList & Set.toList & length) /= length xs
+
+isNotInOrOnce :: [Node] -> Node -> Bool
+isNotInOrOnce path node = case node `elemIndex` path of
+  Nothing -> True
+  Just n -> node `notElem` drop (succ n) path
+
 nodeLarge :: Node -> Bool
 nodeLarge = isUpper . head
 
@@ -38,5 +55,12 @@ part1 path graph@(_, edges) =
       nextSteps = filter (\cave -> nodeLarge cave || cave `notElem` path) potentialNextSteps
    in sum $ map (\cave -> part1 (cave : path) graph) nextSteps
 
+part2 :: [Node] -> Graph -> Int
+part2 ("end" : _) _ = 1
+part2 path graph@(_, edges) =
+  let potentialNextSteps = edges & Map.findWithDefault [] (head path)
+      nextSteps = filter (visitable path) potentialNextSteps
+   in sum $ map (\cave -> part2 (cave : path) graph) nextSteps
+
 main :: IO()
-main = readFile "input.txt" >>= print . part1 ["start"] . prepare
+main = readFile "input.txt" >>= print . part2 ["start"] . prepare
