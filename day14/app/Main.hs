83dd33d6945186cd0a70204cb5ac2cb62f12adbe
joverandout <joverandout@gmail.com>
2021-12-21 21:39:27 +0000

    done but a bit dodge

--- day14/app/Main.hs
+++ day14/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Data.Map as Map
 import Data.List.Split
+import Data.List
 import Lib
 
 type Rules = Map.Map String String
@@ -9,11 +10,13 @@ type Rules = Map.Map String String
 main :: IO ()
 main = someFunc
 
-sequence = "CKKOHNSBPCPCHVNKHFFK"
+sSequence = "CKKOHNSBPCPCHVNKHFFK"
 rules = ["KO -> C","SO -> S","BF -> V","VN -> B","OV -> K","VH -> O","KV -> N","KB -> F","NB -> C","HS -> K","PF -> B","HB -> N","OC -> H","FS -> F","VV -> S","KF -> C","FN -> F","KP -> S","HO -> N","NH -> K","OO -> S","FB -> C","BP -> F","CH -> N","SN -> O","KN -> B","CV -> O","CC -> B","VB -> C","PH -> V","CO -> K","KS -> K","BK -> N","FH -> S","PV -> H","CB -> P","FO -> F","BB -> K","OB -> C","HH -> F","ON -> O","FK -> B","NF -> F","SV -> F","CP -> H","SS -> B","OP -> H","NS -> O","HK -> N","BC -> P","NV -> V","VS -> F","PC -> V","CS -> F","NP -> V","PS -> F","VC -> F","KK -> S","PO -> P","HF -> H","KC -> P","SF -> N","BV -> N","FF -> V","FV -> V","BO -> N","OS -> C","OF -> H","CN -> S","NO -> O","NC -> B","VK -> C","HN -> B","PK -> N","SK -> S","HV -> F","BH -> B","OK -> S","VO -> B","BS -> H","PP -> N","SC -> K","BN -> P","FC -> S","SB -> B","SH -> H","NN -> V","NK -> N","VF -> H","CF -> F","PB -> C","SP -> P","KH -> C","VP -> N","CK -> H","HP -> P","FP -> B","HC -> O","PN -> F","OH -> H"]
 
 testSequence = "NNCB"
 testRules = ["CH -> B","HH -> N","CB -> H","NH -> C","HB -> C","HC -> B","HN -> C","NN -> C","BH -> H","NC -> B","NB -> B","BN -> B","BB -> N","BC -> B","CC -> N","CN -> C"]
+
+rulesMap = rulesToMap rules
 
 rulesToMap :: [String] -> Map String String
 rulesToMap stringys = Map.fromList (splitAllStrings stringys)
@@ -35,8 +38,21 @@ replace [] mapp = ""
 replace [x] mapp = [x]
 replace (x:y:xs) mapp = case Map.lookup [x, y] mapp of
     Nothing -> x : replace (y:xs) mapp
-    Just r -> (x : r) ++ (replace (y : xs) mapp)
+    Just r -> (x : r) ++ replace (y : xs) mapp
 
 part1Steps :: Int -> String -> Map String String -> String
 part1Steps 10 stringy mapp = stringy
 part1Steps x stringy mapp  = part1Steps (x+1) (replace stringy mapp) mapp
+
+part2Steps :: Int -> String -> Map String String -> String
+part2Steps 40 stringy mapp = stringy
+part2Steps x stringy mapp  = part2Steps (x+1) (replace stringy mapp) mapp
+
+part1map :: String -> Map Char Int -> Map Char Int
+part1map [x] mapp = case Map.lookup x mapp of
+    Nothing -> Map.insert x 1 mapp
+    Just r -> Map.insert x (r+1) mapp
+part1map (x:xs) mapp = part1map xs (part1map [x] mapp)
+
+-- part1BigAndSmall :: Map Char Int -> Int -> Int -> Int
+-- part1BigAndSmall mapp big small 
